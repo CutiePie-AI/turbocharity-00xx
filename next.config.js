a22df46c82dd4011ff1d4bac1b0858e5af39d3b1
reetam-ganguli <72358992+reetam-ganguli@users.noreply.github.com>
2026-03-14 18:00:31 -0700

    Initial scaffold

--- next.config.js
+++ next.config.js
@@ -1,25 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "via.placeholder.com",
-      },
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-      },
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-      },
-    ],
-  },
-};
-
-module.exports = nextConfig;
+const nextConfig = {}
+module.exports = nextConfig
